c21495c207ed247ce799c7ed5afc4da37140827c
Vaishnav <vaishnavchandurkar12@gmail.com>
2024-09-02 19:17:21 +0530

    feat: responsive and sticky header

--- src/components/editor/toolbar/Toolbar.tsx
+++ src/components/editor/toolbar/Toolbar.tsx
@@ -43,56 +43,58 @@ const Toolbar = (props: Props) => {
   }, {} as Record<ExtensionKey, Extension>);
 
   return (
-    <div className="border rounded-lg p-1 flex items-center gap-x-1 border-neutral-200 bg-transparent shadow-sm hover:text-neutral-900 dark:border-neutral-800 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 mb-6">
-      {activeExtensions[extensionMap.heading] ? (
-        <Heading editor={editor} />
-      ) : null}
+    <div className="sticky top-10 bg-white z-50 mb-20">
+      <div className="border flex-wrap rounded-lg p-1 flex items-center gap-x-1 border-neutral-200 shadow-sm hover:text-neutral-900 dark:border-neutral-800 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 ">
+        {activeExtensions[extensionMap.heading] ? (
+          <Heading editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.bold] ? <Bold editor={editor} /> : null}
+        {activeExtensions[extensionMap.bold] ? <Bold editor={editor} /> : null}
 
-      {activeExtensions[extensionMap.italic] ? (
-        <Italic editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.italic] ? (
+          <Italic editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.paragraph] ? (
-        <Text editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.paragraph] ? (
+          <Text editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.strike] ? (
-        <Strike editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.strike] ? (
+          <Strike editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.underline] ? (
-        <Underline editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.underline] ? (
+          <Underline editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.codeBlock] ? (
-        <Codeblock editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.codeBlock] ? (
+          <Codeblock editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.bulletList] ? (
-        <BulletList editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.bulletList] ? (
+          <BulletList editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.orderedList] ? (
-        <OrderedList editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.orderedList] ? (
+          <OrderedList editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.taskList] ? (
-        <TodoList editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.taskList] ? (
+          <TodoList editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.blockquote] ? (
-        <Blockquote editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.blockquote] ? (
+          <Blockquote editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.horizontalRule] ? (
-        <Divider editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.horizontalRule] ? (
+          <Divider editor={editor} />
+        ) : null}
 
-      {activeExtensions[extensionMap.textAlign] ? (
-        <TextAlign editor={editor} />
-      ) : null}
+        {activeExtensions[extensionMap.textAlign] ? (
+          <TextAlign editor={editor} />
+        ) : null}
+      </div>
     </div>
   );
 };
